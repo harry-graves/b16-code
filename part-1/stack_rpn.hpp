a72c6694598a170b327b237ac4b4b3da8077574b
Harry Graves <163601735+harry-graves@users.noreply.github.com>
2024-03-21 22:13:24 +0000

    Question 3 Complete

--- part-1/stack_rpn.hpp
+++ part-1/stack_rpn.hpp
@@ -5,6 +5,61 @@
 #include <cassert>
 #include <functional>
 
-// WRITE YOUR CODE HERE
+template <typename T>
+void plus(Stack<T> &stack)
+{
+    T value1 = stack.top();
+    stack.pop();
+
+    T value2 = stack.top();
+    stack.pop();
+    
+    stack.push(value1 + value2);
+};
+
+template <typename T>
+void minus(Stack<T> &stack)
+{
+    T value1 = stack.top();
+    stack.pop();
+
+    T value2 = stack.top();
+    stack.pop();
+
+    stack.push(value1 - value2);
+};
+
+template <typename T>
+void multiplies(Stack<T> &stack)
+{
+    T value1 = stack.top();
+    stack.pop();
+
+    T value2 = stack.top();
+    stack.pop();
+    
+    stack.push(value1 * value2);
+};
+
+template <typename T>
+void divides(Stack<T> &stack)
+{
+    T value1 = stack.top();
+    stack.pop();
+
+    T value2 = stack.top();
+    stack.pop();
+
+    stack.push(value1 / value2);
+};
+
+template <typename T>
+void negates(Stack<T> &stack)
+{
+    T value1 = stack.top();
+    stack.pop();
+
+    stack.push(-value1);    
+};
 
 #endif // __stack_rpn__
